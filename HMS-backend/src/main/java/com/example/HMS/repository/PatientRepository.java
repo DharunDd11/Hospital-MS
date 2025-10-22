b5fdd16652a02842ab149e40528f5a3bee6114e1
Dharun Dobba <147317667+DharunDd11@users.noreply.github.com>
2025-10-23 00:20:00 +0530

    Delete HMS-backend directory

--- HMS-backend/src/main/java/com/example/HMS/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.HMS.repository;
-
-import com.example.HMS.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-}
